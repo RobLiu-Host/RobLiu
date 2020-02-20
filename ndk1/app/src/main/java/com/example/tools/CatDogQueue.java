6ce5dfca8b6da67d855217a700e297fb1ebfabbc
liuba <lb>
2020-02-20 14:10:32 +0800

    完成猫狗队列的编码

--- ndk1/app/src/main/java/com/example/tools/CatDogQueue.java
+++ ndk1/app/src/main/java/com/example/tools/CatDogQueue.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 猫狗队列 要求满足 不修改原有的宠物类 能获取第一个猫 第一支🐶 第一支动物
+ * 猫狗队列 要求满足 不修改原有的宠物类 能获取第一只i猫 第一只狗 第一只动物 队列是否有猫 是否有狗 是有有宠物
  *
  * 实现思路通过计数器获得当前宠物的进去顺序 通过判断两个队列的顶部的count就能判断那个宠物应该被弹出
  */
@@ -29,8 +29,38 @@ public class CatDogQueue {
         }
     }
 
-    void pollAll(){
-
+    public Pet pollAll(){
+            if(!cat.isEmpty()&&!dog.isEmpty()) {
+                if (cat.peek().getCount() < dog.peek().getCount()) {
+                    return cat.poll().getPet();
+                }else {
+                    return dog.poll().getPet();
+                }
+            }else if(cat.isEmpty()&&!dog.isEmpty()){
+                return dog.poll().getPet();
+            }else if(!cat.isEmpty()&&dog.isEmpty()){
+                return cat.poll().getPet();
+            }else {
+               throw new RuntimeException("this queue isEmpty") ;
+            }
     }
+    boolean isCatEmpty(){
+        return cat.isEmpty();
+    }
+
+
+    boolean sDogEmpty(){
+        return dog.isEmpty();
+    }
+
+    boolean isQueueEmpty(){
+       return cat.isEmpty()&&dog.isEmpty();
+    }
+
+
+
+
+
+
 
 }
--- ndk1/app/src/main/java/com/example/tools/PetEnter.java
+++ ndk1/app/src/main/java/com/example/tools/PetEnter.java
@@ -4,8 +4,16 @@ import com.example.entity.Pet;
 
 public class PetEnter {
 
-    Pet pet;
-    long count;
+    private Pet pet;
+    private long count;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public long getCount() {
+        return count;
+    }
 
     public PetEnter(Pet pet, long count) {
         this.pet = pet;
